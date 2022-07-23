8cf83c7aeeb9ec10ea1a17f470241d128d2f5b82
jreyes1990 <jarl1990mt@gmail.com>
2022-07-22 22:52:09 -0600

    Pruebas con ESLint

--- 01 - Introduccion/js/app.js
+++ 01 - Introduccion/js/app.js
@@ -2,3 +2,14 @@
 const nombre = prompt('Cual es tu nombre?');
 // Toma el nombre y lo muestra en pantall
 document.querySelector('.contenido').innerHTML = `${nombre} ESTA APRENDIENDO JAVASCRIPT MODERNO`;
+
+/*
+console.log('Hola');
+console.log('Mundo');
+
+function hola(){
+    console.log('Ok');
+    console.log('Hola');
+    console.log('Mundo');
+}
+*/
