d5bed86bdb8a751545e03af02367c10f76a639a6
jreyes1990 <jarl1990mt@gmail.com>
2022-07-22 20:59:32 -0600

    Comentarios en JavaScript

--- 01 - Introduccion/js/app.js
+++ 01 - Introduccion/js/app.js
@@ -1,2 +1,4 @@
+// Declaracion de variable constante, pregunta al usuario
 const nombre = prompt('Cual es tu nombre?');
+// Toma el nombre y lo muestra en pantall
 document.querySelector('.contenido').innerHTML = `${nombre} ESTA APRENDIENDO JAVASCRIPT MODERNO`;
