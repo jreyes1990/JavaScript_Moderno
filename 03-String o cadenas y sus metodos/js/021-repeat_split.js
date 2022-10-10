dcd60b39c238251908273d6d7115a7e337840b6f
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-09 19:53:15 -0600

    Metodos String: repeat y split

---
+++ 03-String o cadenas y sus metodos/js/021-repeat_split.js
@@ -0,0 +1,20 @@
+const producto = 'Monitor 20 pulgadas';
+
+console.log(producto)
+
+// Va a permitir repetir una cadena de texto
+const texto = ' en promocion'.repeat(3);
+
+console.log(texto)
+console.log(`${producto} ${texto} !!!`)
+
+
+// Dividir un string
+const actividad = 'Estoy aprendiendo JavaScrip Moderno';
+console.log(actividad.split(" "))
+
+const hobbies = 'Leer, Caminar, Escuchar música, Escribir, Aprender a programar';
+console.log(hobbies.split(", "))
+
+const tweet = "Aprendiendo JavaScript #JSModerno";
+console.log(tweet.split('#'))
