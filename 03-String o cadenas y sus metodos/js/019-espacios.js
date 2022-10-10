3d4f91a42dcef0282303fff77cfaceb2bf1ef187
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-09 18:51:44 -0600

    Cortar espacios en blanco de un string

---
+++ 03-String o cadenas y sus metodos/js/019-espacios.js
@@ -0,0 +1,15 @@
+const producto = '          Monitor 20 pulgadas          ';
+
+console.log(producto)
+console.log(producto.length)
+
+// Eliminar espacios del inicio
+console.log(producto.trimStart())
+
+// Eliminar espacios al final
+console.log(producto.trimEnd())
+
+// Eliminar espacio del inicio y final
+console.log(producto.trimStart().trimEnd())
+
+console.log(producto.trim())
