234c3a3353b1781b5c6d05af05c53ae6af99a626
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-09 21:48:20 -0600

    Metodos String: toUpperCase y toLowerCase

---
+++ 03-String o cadenas y sus metodos/js/022-Mayuscula_Minuscula.js
@@ -0,0 +1,18 @@
+const producto = 'Monitor 20 pulgadas';
+
+console.log(producto)
+
+console.log(`Uso de toUpperCase(): ${producto.toUpperCase()}`)
+console.log(`Uso de toLowerCase(): ${producto.toLowerCase()}`)
+
+
+const email = "jreyesl1990mt@gmail.com";
+
+console.log(`Uso de toUpperCase(): ${email.toUpperCase()}`)
+console.log(`Uso de toLowerCase(): ${email.toLowerCase()}`)
+
+
+const precio = 300;
+
+console.log(precio)
+console.log(`Uso de toString(): ${precio.toString()}`)
