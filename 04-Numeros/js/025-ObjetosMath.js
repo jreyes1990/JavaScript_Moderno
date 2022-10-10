0cc79db08d88c7806842c5311c1eb37786858333
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-09 23:04:19 -0600

    Objetos Math

---
+++ 04-Numeros/js/025-ObjetosMath.js
@@ -0,0 +1,34 @@
+console.log(Math)
+
+// PI
+console.log(`PI: ${Math.PI}`)
+
+// Redonder
+console.log(`round: ${Math.round(2.8)}`)
+
+// Redondear hacia arriba
+console.log(`ceil: ${Math.ceil(2.4)}`)
+
+// Redondear hacia abajo
+console.log(`floor: ${Math.floor(2.9)}`)
+
+// Raiz cuadrada
+console.log(`sqrt: ${Math.sqrt(144)}`)
+
+// Valor absoluto
+console.log(`abs: ${Math.abs(-500)}`)
+
+// Potencia
+console.log(`pow: ${Math.pow(2,5)}`)
+
+// Minimo
+console.log(`min: ${Math.min(3,5,1,12,-3)}`)
+
+// Maximo
+console.log(`max: ${Math.max(3,5,1,12,-3)}`)
+
+// Aleatorio
+console.log(`random: ${Math.random()*20}`)
+
+// Aleatorio dentro un rango
+console.log(`random: ${Math.floor(Math.random()*30)}`)
