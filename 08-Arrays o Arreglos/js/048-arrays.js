8085df626cbc5df82fe0d665a43245b8fd2250a1
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-11 22:59:34 -0600

    Acceder a los valores de un Array

--- 08-Arrays o Arreglos/js/048-arrays.js
+++ 08-Arrays o Arreglos/js/048-arrays.js
@@ -1,12 +1,19 @@
 const numeros = [10, 20, 30];
 
-console.log(numeros)
+console.log(numeros);
 
 // Un arreglo que contiene datos de todo tipo
-const deTodo = ["Hola",10,true,"si",null,{nombre: "Jolberth", trabajo: "Programador"}];
+const deTodo = [
+  "Hola",
+  10,
+  true,
+  "si",
+  null,
+  { nombre: "Jolberth", trabajo: "Programador" },
+];
 
-console.log(deTodo)
-console.log(deTodo[2])
-console.log(deTodo[5].nombre)
+console.log(deTodo);
+console.log(deTodo[2]);
+console.log(deTodo[5].nombre);
 
-console.table(deTodo)
+console.table(deTodo);
