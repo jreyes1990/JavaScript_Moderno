9f41c87308d7ca41067e5dc9af0d6ef4363d3188
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-12 22:58:22 -0600

    Añadir nuevos elementos, al inicio o final de un Array

--- 08-Arrays o Arreglos/js/050-recorrer_array.js
+++ 08-Arrays o Arreglos/js/050-recorrer_array.js
@@ -6,3 +6,32 @@ console.table(meses)
 for(let i=0; i<meses.length; i++){
     console.log(`${i} ${meses[i]}`)
 }
+
+
+/*#########################################################################################################################*/
+
+const carrito = [];
+
+// Definir un producto
+const producto = {
+    nombre: 'Monitor 32 pulgadas',
+    precio: 400
+}
+
+const producto2 = {
+    nombre: 'Celular',
+    precio: 800
+}
+
+carrito.push(producto);
+carrito.push(producto2);
+
+
+const producto3 = {
+    nombre: 'Teclado',
+    precio: 50
+}
+
+carrito.unshift(producto3);
+
+console.table(carrito)
