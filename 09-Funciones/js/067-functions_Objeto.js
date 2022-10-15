15dfc7c5e4fe615309c7bb46067ae2db31a9f2d5
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-15 17:21:49 -0600

    Añadir funciones a un objeto

---
+++ 09-Funciones/js/067-functions_Objeto.js
@@ -0,0 +1,26 @@
+const reproductor = {
+  reproducir: function (id) {
+    console.log(`Reproduciendo cancion con el id ${id}`)
+  },
+  pausar: function(){
+    console.log('Pausando .....')
+  },
+  borrar: function(id){
+    console.log(`Borrando cancion..... ${id}`)
+  },
+  crearPlayList: function(nombre){
+    console.log(`Creando la playlist de ${nombre}`)
+  },
+  reproducirPlayList: function(nombre){
+    console.log(`Reproduciendo la playlist ${nombre}`)
+  }
+}
+
+
+reproductor.reproducir(30);
+reproductor.reproducir(20);
+reproductor.pausar();
+reproductor.borrar(30);
+reproductor.crearPlayList('Heavy Metal');
+reproductor.crearPlayList('Rock 90s');
+reproductor.reproducirPlayList('Heavy Metal');
