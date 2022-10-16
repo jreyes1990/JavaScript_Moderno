e9c0d1a15b1789b505cd48ce198cb1d4ba3b837b
Jolberth Reyes <jarl1990mt@gmail.com>
2022-10-15 23:00:11 -0600

    Arrow functions en el reproductor de musica

--- 09-Funciones/js/067-functions_Objeto.js
+++ 09-Funciones/js/067-functions_Objeto.js
@@ -1,4 +1,6 @@
 const reproductor = {
+  cancion: '',
+
   reproducir: function (id) {
     console.log(`Reproduciendo cancion con el id ${id}`)
   },
@@ -13,9 +15,20 @@ const reproductor = {
   },
   reproducirPlayList: function(nombre){
     console.log(`Reproduciendo la playlist ${nombre}`)
+  },
+
+  set nuevaCancion(cancion){
+    this.cancion = cancion;
+    console.log(`Añadiendo ${cancion}`)
+  },
+  get obtenerCancion(){
+    console.log(`${this.cancion}`)
   }
 }
 
+
+reproductor.nuevaCancion = 'Enter Sandman';
+reproductor.obtenerCancion;
 
 reproductor.reproducir(30);
 reproductor.reproducir(20);
