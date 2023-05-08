49e3991c80ce75ebb29cb82b46862c1ad472e6c3
jreyes1990 <jarl1990mt@gmail.com>
2023-05-07 23:27:19 -0600

    Uso de querySelector

---
+++ 13-Document Object Model (DOM)/js/100-querySelector.js
@@ -0,0 +1,18 @@
+const card = document.querySelector('.card');
+console.log(card);
+
+// Podemos tenes selectores especificos como en CSS
+const infor = document.querySelector('.premium .info');
+console.log(infor);
+
+// Seleccionar el segundo card de hospedaje
+const segundoCard = document.querySelector('section.hospedaje .card:nth-child(2)');
+console.log(segundoCard);
+
+// Seleccionar el formulario
+const form = document.querySelector('#formulario:nth-child(2)');
+console.log(form);
+
+// Seleccionar elementos HTML
+const naveg = document.querySelector('nav');
+console.log(naveg);
