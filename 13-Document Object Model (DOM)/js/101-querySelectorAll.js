27b9392b4495910525383f2009b35c62499d54ae
jreyes1990 <jarl1990mt@gmail.com>
2023-05-08 22:05:36 -0600

    Uso de querySelectorAll

---
+++ 13-Document Object Model (DOM)/js/101-querySelectorAll.js
@@ -0,0 +1,10 @@
+const card = document.querySelectorAll('.card');
+console.log(card);
+
+// Seleccionar el formulario
+const form = document.querySelectorAll('#formulario');
+console.log(form);
+
+// Si un elemento no existe
+const noExiste = document.querySelectorAll('no-existe');
+console.log(noExiste);
