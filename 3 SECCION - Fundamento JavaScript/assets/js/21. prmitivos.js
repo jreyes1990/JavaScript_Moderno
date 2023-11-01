53be6bf17dd5ede4293328b58952192db5433610
jreyes1990 <jarl1990mt@gmail.com>
2023-11-01 17:54:32 -0600

    21. Introducción general a los tipos primitivos

---
+++ 3 SECCION - Fundamento JavaScript/assets/js/21. prmitivos.js
@@ -0,0 +1,30 @@
+let nombre ='Peter Parker';
+console.log({nombre});
+
+nombre = 'Ben Parker';
+console.log({nombre});
+console.log(typeof nombre);
+
+nombre = 123456;
+console.log({nombre});
+console.log(typeof nombre);
+
+nombre = 200.961;
+console.log({nombre});
+console.log(typeof nombre);
+
+let esMarvel = false;
+console.log({esMarvel});
+console.log(typeof esMarvel);
+
+let superPoder;
+console.log({superPoder});
+console.log(typeof superPoder);
+
+let soyNull = null;
+console.log({soyNull});
+console.log(typeof soyNull);
+
+let simbol1 = Symbol('a');
+console.log({simbol1});
+console.log(typeof simbol1);
