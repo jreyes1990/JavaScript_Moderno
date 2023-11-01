37e6d2d022171fa93662838793d3a8b35e445738
jreyes1990 <jarl1990mt@gmail.com>
2023-10-31 22:48:37 -0600

    11. Introducción a variables y comentarios

---
+++ 2 SECCION - Introduccion/11. variables.js
@@ -0,0 +1,13 @@
+// Comentarios
+/* Comentarios */
+
+let   a = 10; // Forma actual de crear variables
+var   b = 10; // Forma antigua de crear variables
+const c = 10;
+
+// c = 20; // No se puede hacer
+
+a = 20;
+b = 30;
+
+console.log(`a = ${a} y b = ${b}`);
