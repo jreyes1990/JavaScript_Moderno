caf7cd85c22995e21e6cee25c43435aebc272ab0
jreyes1990 <jarl1990mt@gmail.com>
2023-10-31 23:02:05 -0600

    12. Introducción a la consola

--- 2 SECCION - Introduccion/11. variables.js
+++ 2 SECCION - Introduccion/11. variables.js
@@ -4,6 +4,7 @@
 let   a = 10; // Forma actual de crear variables
 var   b = 10; // Forma antigua de crear variables
 const c = 10;
+let   d = 'Spiderman';
 
 // c = 20; // No se puede hacer
 
@@ -11,3 +12,9 @@ a = 20;
 b = 30;
 
 console.log(`a = ${a} y b = ${b}`);
+
+console.log('%c Mis variables', 'color: blue; font-weight: bold'); // Aplicando CSS al console.log
+console.warn({b}); // Mensaje de alerta
+console.error({a}); // Mensaje de error
+console.info({c});
+console.table({d}); // Mensaje dentro de una tabla
