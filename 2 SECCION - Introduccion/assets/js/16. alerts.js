bd7e4c588442dd3def3a62a1fd801c6c8f10401a
jreyes1990 <jarl1990mt@gmail.com>
2023-11-01 16:31:55 -0600

    16. Prompt, confirm y alert

---
+++ 2 SECCION - Introduccion/assets/js/16. alerts.js
@@ -0,0 +1,8 @@
+alert('Hola mundo');
+
+let nombre = prompt('Cual es tu nombre?','Sin nombre');
+let nombreCompleto = prompt('Cual es tu nombre completo?');
+console.log('***** '+nombreCompleto+' *****');
+
+let seleccion = confirm('Esta seguro de borrar esto?');
+console.log('***** '+seleccion+' *****');
