c80336fee0e9c07d40cc2304c3ce47763d6c5896
jreyes1990 <jarl1990mt@gmail.com>
2023-11-04 16:50:40 -0600

    51. Pedir carta

--- Fernando Herrera/5 SECCION - Laboratorio 1 (Blackjack)/assets/js/juego.js
+++ Fernando Herrera/5 SECCION - Laboratorio 1 (Blackjack)/assets/js/juego.js
@@ -10,6 +10,7 @@ let deck = [];
 const tipos = ['C','D','H','S'];
 const especiales = ['A','J','Q','K'];
 
+// TODO: Esta funcion crea una nueva baraja
 const crearDeck = () => {
   for (let i = 2; i < 10; i++) {
     for (let tipo of tipos) {
@@ -22,8 +23,25 @@ const crearDeck = () => {
      deck.push(esp+tipo); 
     } 
   }
-  console.log(deck);
+  // console.log(deck);
   deck = _.shuffle(deck);
   console.log(deck);
+
+  return deck;
 }
 crearDeck();
+
+// TODO: Esta funcion me permite tomar una carta
+const pedirCarta = () => {
+  if (deck.length === 0) {
+    throw 'No hay cartas en el deck';
+  }
+  const carta = deck.pop(); // TODO: pop() -> Remueve el ultimo elemento y lo regresa
+  console.log(deck);
+  console.log(carta);
+  return carta;
+}
+
+for (let i = 0; i < 100; i++) {
+  pedirCarta();  
+}
