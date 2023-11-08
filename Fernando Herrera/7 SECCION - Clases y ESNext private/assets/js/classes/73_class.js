15cc3f920f2a65a6468b78d6cf81c3da8290fcac
jreyes1990 <jarl1990mt@gmail.com>
2023-11-07 21:21:53 -0600

    74. Métodos en las clases

--- Fernando Herrera/7 SECCION - Clases y ESNext private/assets/js/classes/73_class.js
+++ Fernando Herrera/7 SECCION - Clases y ESNext private/assets/js/classes/73_class.js
@@ -9,9 +9,23 @@ class Persona{
     this.codigo = codigo;
     this.frase = frase;
   }
+
+  // TODO: Creacion de metodo dentro de las clases
+  quienSoy(){
+    console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
+  }
+
+  miFrase(){
+    this.quienSoy();
+    console.log(`${this.codigo} dice: ${this.frase}`);
+  }
 }
 
 const spiderman = new Persona('Peter Parker','Spiderman','Soy tu amable vecino Spiderman');
 const iroman = new Persona('Tony Startk','Iroman','Yo soy Iroman');
 console.log(spiderman);
 console.log(iroman);
+spiderman.quienSoy();
+iroman.quienSoy();
+spiderman.miFrase();
+iroman.miFrase();
