7f4262d1b43e4f27bfd42f0719e0028fe3abd6b0
jreyes1990 <jarl1990mt@gmail.com>
2023-11-07 21:37:47 -0600

    75. Sets y Gets

--- Fernando Herrera/7 SECCION - Clases y ESNext private/assets/js/classes/73_class.js
+++ Fernando Herrera/7 SECCION - Clases y ESNext private/assets/js/classes/73_class.js
@@ -3,11 +3,21 @@ class Persona{
   nombre ='';
   codigo ='';
   frase ='';
+  comida = '';
 
   constructor(nombre='Sin nombre', codigo='Sin codigo', frase='Sin frase'){
     this.nombre = nombre;
     this.codigo = codigo;
     this.frase = frase;
+  }
+
+  // TODO: Uso de Sets (Establecer un valor) y Gets (Recuperar un valor)
+  set setComidaFavorita(comida){
+    this.comida = comida.toUpperCase();
+  }
+
+  get getComidaFavorita(){
+    return `La comida favorita de ${this.nombre} es ${this.comida}`;
   }
 
   // TODO: Creacion de metodo dentro de las clases
@@ -25,7 +35,13 @@ const spiderman = new Persona('Peter Parker','Spiderman','Soy tu amable vecino S
 const iroman = new Persona('Tony Startk','Iroman','Yo soy Iroman');
 console.log(spiderman);
 console.log(iroman);
+
 spiderman.quienSoy();
 iroman.quienSoy();
+
 spiderman.miFrase();
 iroman.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tia May';
+console.log(spiderman);
+console.log(spiderman.getComidaFavorita);
