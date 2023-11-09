2242453a4960f90e35c926716cc652a9cc2ef85b
jreyes1990 <jarl1990mt@gmail.com>
2023-11-08 23:11:51 -0600

    80. Protip: Multiples constructores

---
+++ Fernando Herrera/7 SECCION - Clases y ESNext private/assets/js/classes/80_constructores_multiples_protip.js
@@ -0,0 +1,30 @@
+class Persona{
+  static porObjeto({nombre, apellido, pais}){
+    return new Persona(nombre, apellido, pais);
+  }
+
+  constructor(nombre, apellido, pais){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.pais = pais;
+  }
+
+  getInfo(){
+    console.log(`info ${this.nombre}, ${this.apellido}, ${this.pais}`);
+  }
+}
+
+const nombre1   = 'Melissa',
+      apellido1 = 'Flores',
+      pais1     = 'Honduras';
+
+const jreyes = {
+  nombre: 'Jolberth',
+  apellido: 'Reyes',
+  pais: 'Guatemala',
+}
+
+const persona1 = new Persona(nombre1, apellido1, pais1);
+const persona2 = Persona.porObjeto(jreyes);
+persona1.getInfo();
+persona2.getInfo();
