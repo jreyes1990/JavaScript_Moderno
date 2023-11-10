cfabec55129a323fa18bdfed8138ac09e69ebd01
jreyes1990 <jarl1990mt@gmail.com>
2023-11-09 23:12:26 -0600

    86. Ejecutar y explicar el proyecto

--- Fernando Herrera/8 SECCION - Modulo y Vite/vite_app/main.js
+++ Fernando Herrera/8 SECCION - Modulo y Vite/vite_app/main.js
@@ -1,15 +1,15 @@
-
-// import javascriptLogo from './javascript.svg';
-// import viteLogo from './vite.svg'
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="" class="logo" alt="Vite logo" />
+      <img src="${javascriptLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="" class="logo vanilla" alt="JavaScript logo" />
+      <img src="${viteLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
@@ -19,6 +19,6 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
 setupCounter(document.querySelector('#counter'))
