1bdb27858fff73f5afde029ae6821063b3055075
jreyes1990 <jarl1990mt@gmail.com>
2023-11-11 17:52:54 -0600

    92. Mejorar la documentación JSDoc Comments

--- Fernando Herrera/8 SECCION - Modulo y Vite/blackjack_vite/src/blackjack/index.js
+++ Fernando Herrera/8 SECCION - Modulo y Vite/blackjack_vite/src/blackjack/index.js
@@ -11,130 +11,130 @@ import {crearDeck} from './usecases/crear_deck';
  * 2S = Two of Spades (Espadas)
 */
 
-  let deck = [];
-  const tipos = ['C','D','H','S'],
-        especiales = ['A','J','Q','K'];
-  let puntosJugadores = [];
+let deck = [];
+const tipos = ['C','D','H','S'],
+      especiales = ['A','J','Q','K'];
+let puntosJugadores = [];
 
-  // TODO: Referencias del HTML
-  const btnPedir = document.querySelector('#btnPedir'),
-        btnDetener = document.querySelector('#btnDetener'),
-        btnNuevo = document.querySelector('#btnNuevo');
-  const divCartasJugadores = document.querySelectorAll('.divCartas');
-  const smalls = document.querySelectorAll('small');
+// TODO: Referencias del HTML
+const btnPedir = document.querySelector('#btnPedir'),
+      btnDetener = document.querySelector('#btnDetener'),
+      btnNuevo = document.querySelector('#btnNuevo');
+const divCartasJugadores = document.querySelectorAll('.divCartas');
+const smalls = document.querySelectorAll('small');
 
-  btnPedir.disabled = true;
-  btnDetener.disabled = true;
+btnPedir.disabled = true;
+btnDetener.disabled = true;
 
-  // TODO: Esta funcion inicializa el juego
-  const inicializarJuego = (numeroJugadores = 2) => {
-    deck = crearDeck(tipos, especiales);
+// TODO: Esta funcion inicializa el juego
+const inicializarJuego = (numeroJugadores = 2) => {
+  deck = crearDeck(tipos, especiales);
 
-    puntosJugadores = [];
+  puntosJugadores = [];
 
-    for (let i = 0; i < numeroJugadores; i++) {
-      puntosJugadores.push(0);
+  for (let i = 0; i < numeroJugadores; i++) {
+    puntosJugadores.push(0);
+  }
+
+  smalls.forEach(elem => elem.innerHTML = 0);
+  divCartasJugadores.forEach(elem => elem.innerHTML = '');
+
+  btnPedir.disabled = false;
+  btnDetener.disabled = false;
+}
+
+// TODO: Esta funcion me permite tomar una carta
+const pedirCarta = () => {
+  if (deck.length === 0) {
+    throw 'No hay cartas en el deck';
+  }
+  
+  return deck.pop(); // TODO: pop() -> Remueve el ultimo elemento y lo regresa
+}
+
+// TODO: Esta funcion me permite obtener el valor de una carta
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length-1);
+  
+  return (isNaN(valor)) ? (valor==='A') ? 11 : 10 : Number(valor);
+}
+
+// TODO: Esta funcion me permite acumular los puntos de los jugadores, TURNO: [0]=Primer jugador y el ultimo de la computadora
+const acumularPuntos = (carta, turno) => {
+  puntosJugadores[turno] = puntosJugadores[turno]+valorCarta(carta);
+  smalls[turno].innerHTML = puntosJugadores[turno];
+  return puntosJugadores[turno];
+}
+
+// TODO: Esta funcion me permite crear las cartas
+const crearCartas = (carta, turno) => {
+  const imgCarta = document.createElement('img');
+  imgCarta.src = `assets/cartas/${carta}.png`;
+  imgCarta.classList.add('carta');
+  divCartasJugadores[turno].append(imgCarta);
+}
+
+// TODO: Esta funcion evaluara el jugador ganador o perdedor
+const determinarGanador = () => {
+  const [puntosMinimos, puntosComputadora] = puntosJugadores;
+
+  setTimeout(() => {
+    if(puntosComputadora === puntosMinimos) {
+      alert('Nadie gana :(');
+    } else if (puntosMinimos > 21) {
+      alert('Computadora gana')
+    } else if(puntosComputadora > 21 ||  puntosComputadora > puntosMinimos) {
+      alert('Jugador Gana');
+    } else {
+      alert('Computadora Gana')
     }
+  }, 150);
+}
 
-    smalls.forEach(elem => elem.innerHTML = 0);
-    divCartasJugadores.forEach(elem => elem.innerHTML = '');
+// TODO: Se realizara la funcion para el turno de la computadora
+const turnoComputadora = (puntosMinimos) => {
+  let puntosComputadora = 0;
 
-    btnPedir.disabled = false;
-    btnDetener.disabled = false;
-  }
-
-  // TODO: Esta funcion me permite tomar una carta
-  const pedirCarta = () => {
-    if (deck.length === 0) {
-      throw 'No hay cartas en el deck';
-    }
-    
-    return deck.pop(); // TODO: pop() -> Remueve el ultimo elemento y lo regresa
-  }
-
-  // TODO: Esta funcion me permite obtener el valor de una carta
-  const valorCarta = (carta) => {
-    const valor = carta.substring(0, carta.length-1);
-    
-    return (isNaN(valor)) ? (valor==='A') ? 11 : 10 : Number(valor);
-  }
-
-  // TODO: Esta funcion me permite acumular los puntos de los jugadores, TURNO: [0]=Primer jugador y el ultimo de la computadora
-  const acumularPuntos = (carta, turno) => {
-    puntosJugadores[turno] = puntosJugadores[turno]+valorCarta(carta);
-    smalls[turno].innerHTML = puntosJugadores[turno];
-    return puntosJugadores[turno];
-  }
-
-  // TODO: Esta funcion me permite crear las cartas
-  const crearCartas = (carta, turno) => {
-    const imgCarta = document.createElement('img');
-    imgCarta.src = `assets/cartas/${carta}.png`;
-    imgCarta.classList.add('carta');
-    divCartasJugadores[turno].append(imgCarta);
-  }
-
-  // TODO: Esta funcion evaluara el jugador ganador o perdedor
-  const determinarGanador = () => {
-    const [puntosMinimos, puntosComputadora] = puntosJugadores;
-
-    setTimeout(() => {
-      if(puntosComputadora === puntosMinimos) {
-        alert('Nadie gana :(');
-      } else if (puntosMinimos > 21) {
-        alert('Computadora gana')
-      } else if(puntosComputadora > 21 ||  puntosComputadora > puntosMinimos) {
-        alert('Jugador Gana');
-      } else {
-        alert('Computadora Gana')
-      }
-    }, 150);
-  }
-
-  // TODO: Se realizara la funcion para el turno de la computadora
-  const turnoComputadora = (puntosMinimos) => {
-    let puntosComputadora = 0;
-
-    do {
-      const carta = pedirCarta();
-      puntosComputadora = acumularPuntos(carta, puntosJugadores.length-1);
-      crearCartas(carta, puntosJugadores.length-1);
-
-      if (puntosMinimos > 21) {
-        break;
-      }
-    } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
-    determinarGanador();
-  }
-
-  // TODO: Eventos de boton
-  btnPedir.addEventListener('click', () => {
+  do {
     const carta = pedirCarta();
-    const puntosJugador = acumularPuntos(carta, 0);
-    crearCartas(carta, 0);
+    puntosComputadora = acumularPuntos(carta, puntosJugadores.length-1);
+    crearCartas(carta, puntosJugadores.length-1);
 
-    if (puntosJugador > 21) {
-      console.log('Lo siento mucho, ya perdiste');
-      btnPedir.disabled = true;
-      btnDetener.disabled = true;
-      turnoComputadora(puntosJugador);
-    }else if (puntosJugador === 21){
-      console.log('Genial, lo has logrado');
-      btnPedir.disabled = true;
-      btnDetener.disabled = true;
-      turnoComputadora(puntosJugador);
+    if (puntosMinimos > 21) {
+      break;
     }
-  });
+  } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+  determinarGanador();
+}
 
-  btnDetener.addEventListener('click', () => {
+// TODO: Eventos de boton
+btnPedir.addEventListener('click', () => {
+  const carta = pedirCarta();
+  const puntosJugador = acumularPuntos(carta, 0);
+  crearCartas(carta, 0);
+
+  if (puntosJugador > 21) {
+    console.log('Lo siento mucho, ya perdiste');
     btnPedir.disabled = true;
     btnDetener.disabled = true;
-    turnoComputadora(puntosJugadores[0]);
-  });
+    turnoComputadora(puntosJugador);
+  }else if (puntosJugador === 21){
+    console.log('Genial, lo has logrado');
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
+  }
+});
 
-  btnNuevo.addEventListener('click', () => {
-    console.clear();
-    inicializarJuego();
-  });
+btnDetener.addEventListener('click', () => {
+  btnPedir.disabled = true;
+  btnDetener.disabled = true;
+  turnoComputadora(puntosJugadores[0]);
+});
+
+btnNuevo.addEventListener('click', () => {
+  console.clear();
+  inicializarJuego();
+});
 
  
--- Fernando Herrera/8 SECCION - Modulo y Vite/blackjack_vite/src/blackjack/usecases/crear_deck.js
+++ Fernando Herrera/8 SECCION - Modulo y Vite/blackjack_vite/src/blackjack/usecases/crear_deck.js
@@ -1,7 +1,14 @@
 import _ from 'underscore';
 
-// TODO: Esta funcion crea una nueva baraja
+/**
+ * Esta funcion crea una nueva baraja
+ * @param {Array<String>} tipoDeCartas Ejemplo: ['C','D','H','S']
+ * @param {Array<String>} tipoEspeciales Ejemplo: ['A','J','Q','K']
+ * @returns {Array<String>} retorna un nuevo deck de cartas
+ */
 export const crearDeck = (tipoDeCartas, tipoEspeciales) => {
+  if (!tipoDeCartas || tipoDeCartas.length === 0) throw new Error('TipoDeCartas es obligatorio como un arreglo de string');
+  
   let deck = [];
 
   for (let i = 2; i <= 10; i++) {
