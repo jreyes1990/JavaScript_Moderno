cf895ea43a87ec795cbda5cd8aebc190bf7a5b9f
jreyes1990 <jarl1990mt@gmail.com>
2023-11-13 22:30:25 -0600

    116. Validaciones y optimizaciones

--- Fernando Herrera/10 SECCION - Laboratorio Vite (Lista de Tareas)/tareas_app/src/tareas/use_cases/create_tarea_html.js
+++ Fernando Herrera/10 SECCION - Laboratorio Vite (Lista de Tareas)/tareas_app/src/tareas/use_cases/create_tarea_html.js
@@ -7,9 +7,23 @@ import { Tarea } from "../models/tarea.model";
 export const createTareaHTML = (tarea) => {
   if(!tarea) throw new Error('a TAREA object is required');
 
-  const html = `<h1>${tarea.descripcion}</h1>`;
+  const {done, descripcion, id} = tarea;
+
+  const html = `
+    <div class="view">
+      <input class="toggle" type="checkbox" ${done ? 'checked' : ''}>
+      <label>${descripcion}</label>
+      <button class="destroy"></button>
+    </div>
+    <input class="edit" value="Create a TodoMVC template">
+  `;
   const liElement = document.createElement('li');
   liElement.innerHTML = html;
+  liElement.setAttribute('data-id',id);
+
+  if(tarea.done){
+    liElement.classList.add('completed');
+  }
   
   return liElement;
 }
--- Fernando Herrera/10 SECCION - Laboratorio Vite (Lista de Tareas)/tareas_app/src/tareas/use_cases/render_tareas.js
+++ Fernando Herrera/10 SECCION - Laboratorio Vite (Lista de Tareas)/tareas_app/src/tareas/use_cases/render_tareas.js
@@ -1,15 +1,22 @@
 import { Tarea } from "../models/tarea.model";
 import { createTareaHTML } from "./create_tarea_html";
 
+let element;
+
 /**
  * 
  * @param {String} elementoId 
  * @param {Tarea} tareas 
  */
 export const renderTareas = (elementoId, tareas= []) => {
-  // TODO: Referencia
-  const element = document.querySelector(elementoId);
+  if(!element){
+    element = document.querySelector(elementoId);
+  }
 
+  if(!element) throw new Error(`Element ${elementoId} not found`);
+
+  element.innerHTML = '';
+  
   tareas.forEach(tarea => {
     element.append(createTareaHTML(tarea));
   });
