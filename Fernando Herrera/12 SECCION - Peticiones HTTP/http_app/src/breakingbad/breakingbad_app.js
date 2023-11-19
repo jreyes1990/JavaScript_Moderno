9a8bdf086d1c1aea09e0de9c401afa204acd3048
jreyes1990 <jarl1990mt@gmail.com>
2023-11-18 23:32:01 -0600

    152. BreakingBad component

---
+++ Fernando Herrera/12 SECCION - Peticiones HTTP/http_app/src/breakingbad/breakingbad_app.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const BreakingbadApp = (element) => {
+  document.querySelector('#app_title').innerHTML = 'BreakingBad App';
+  element.innerHTML = 'Loading.....';
+}
