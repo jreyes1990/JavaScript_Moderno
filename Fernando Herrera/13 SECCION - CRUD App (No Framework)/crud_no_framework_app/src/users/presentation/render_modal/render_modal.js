5f7321e2da8ad6aa4ac30bb8a54d135db0e7d582
jreyes1990 <jarl1990mt@gmail.com>
2023-11-23 23:03:22 -0600

    172. Tomar la data del formulario

--- Fernando Herrera/13 SECCION - CRUD App (No Framework)/crud_no_framework_app/src/users/presentation/render_modal/render_modal.js
+++ Fernando Herrera/13 SECCION - CRUD App (No Framework)/crud_no_framework_app/src/users/presentation/render_modal/render_modal.js
@@ -12,6 +12,7 @@ export const hideModal = () => {
   modal?.classList.add('hide-modal');
 
   // TODO: Reset del formulario
+  form?.reset();
 }
 
 /**
@@ -34,7 +35,26 @@ export const renderModal = (element) => {
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log('Formulario Enviado');
+    const formData = new FormData(form);
+    const userLike = {};
+
+    for (const [key, value] of formData) {
+      if(key === 'balance'){
+        userLike[key] = Number(value);
+        continue;
+      }
+
+      if(key === 'isActive'){
+        userLike[key] = (value === 'on') ? true : false;
+        continue;
+      }
+
+      userLike[key] = value;
+    }
+
+    console.log(userLike);
+    // TODO: guardar usuario
+    hideModal();
   });
 
   element.append(modal);
