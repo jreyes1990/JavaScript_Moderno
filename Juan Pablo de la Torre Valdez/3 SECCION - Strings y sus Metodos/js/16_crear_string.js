03ccbd95227f7a12ae6ad449f5465a9c238510fd
jreyes1990 <jarl1990mt@gmail.com>
2023-11-25 00:57:58 -0600

    16. Crear Strings en JavaScript

---
+++ Juan Pablo de la Torre Valdez/3 SECCION - Strings y sus Metodos/js/16_crear_string.js
@@ -0,0 +1,8 @@
+const producto = "Monitor 20 pulgadas"
+console.log(producto);
+
+const producto2 = String("Monitor 24 pulgadas");
+console.log(producto2);
+
+const producto3 = new String("Monitor 27 pulgadas");
+console.log(producto3);
