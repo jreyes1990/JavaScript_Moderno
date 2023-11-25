262e14baf3f6da0dbef7df039838afca02e2ea5e
jreyes1990 <jarl1990mt@gmail.com>
2023-11-25 02:03:38 -0600

    17. String Methods - includes y length

---
+++ Juan Pablo de la Torre Valdez/3 SECCION - Strings y sus Metodos/js/17_string_method.js
@@ -0,0 +1,6 @@
+const producto = "Monitor 20 pulgadas";
+
+// TODO: Conocer la cantidad de letras de la cadena de texto
+console.log(`(length) Cantidad de letras: ${producto.length}`);
+console.log(`(indexOf) Busqueda posicion "pulgadas": ${producto.indexOf("pulgadas")}`);
+console.log(`(includes) Existe "Monitor": ${producto.includes("Monitor")}`);
