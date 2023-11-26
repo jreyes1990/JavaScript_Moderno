2702d4b02510ed2ad7614b3520acf2dedb0023f9
jreyes1990 <jarl1990mt@gmail.com>
2023-11-26 01:16:46 -0600

    22. String Methods - Convertir a Mayusculas o Minusculas

---
+++ Juan Pablo de la Torre Valdez/3 SECCION - Strings y sus Metodos/js/22_string_method_mayus_minus.js
@@ -0,0 +1,16 @@
+const producto = "Monitor 20 pulgadas";
+console.log(producto);
+
+// TODO: Permite convertir la cadena de texto a mayuscula
+console.log('******** Uso de toUpperCase(texto) ********');
+console.log(producto.toUpperCase());
+
+// TODO: Permite convertir la cadena de texto a minuscula
+console.log('******** Uso de toLowerCase(texto) ********');
+console.log(producto.toLowerCase());
+
+// TODO: Permite un numero a String
+console.log('******** Uso de toString(numero) ********');
+const precio = 105.98;
+console.log(precio);
+console.log(precio.toString());
