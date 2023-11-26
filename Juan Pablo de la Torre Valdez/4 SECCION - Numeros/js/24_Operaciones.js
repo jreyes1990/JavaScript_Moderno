b1d3b908bfc35b9560458ef066e56807eb4a0062
jreyes1990 <jarl1990mt@gmail.com>
2023-11-26 02:43:49 -0600

    24. Operaciones en JavaScript

---
+++ Juan Pablo de la Torre Valdez/4 SECCION - Numeros/js/24_Operaciones.js
@@ -0,0 +1,16 @@
+const numero1 = 30;
+const numero2 = 20;
+
+let suma, resta, multiplicacion, division, modulo;
+
+suma = numero1+numero2;
+resta = numero1-numero2;
+multiplicacion = numero1*numero2;
+division = numero1/numero2;
+modulo = numero1%numero2;
+
+console.log(`Suma: ${numero1} + ${numero2} = ${suma}`);
+console.log(`Resta: ${numero1} - ${numero2} = ${resta}`);
+console.log(`Multiplicacion: ${numero1} * ${numero2} = ${multiplicacion}`);
+console.log(`Divison: ${numero1} / ${numero2} = ${division}`);
+console.log(`Modulo: ${numero1} % ${numero2} = ${modulo}`);
