0ffa72140a185543c0ca4be801eb9a89abbcc976
jreyes1990 <jarl1990mt@gmail.com>
2023-11-26 01:08:56 -0600

    21. String Methods - Repeat y Split

---
+++ Juan Pablo de la Torre Valdez/3 SECCION - Strings y sus Metodos/js/21_string_method_Repeat_Split.js
@@ -0,0 +1,15 @@
+const producto = "Monitor 20 pulgadas";
+console.log(producto);
+
+// TODO: Permite repetir una cadena de texto
+console.log('******** Uso de repeat(repetir) ********');
+const texto = ' en promocion'.repeat(3);
+console.log(texto);
+
+// TODO: Permite dividir una cadena de texto
+console.log('******** Uso de split(campoQuePermitaDividir) ********');
+const actividad = "Estoy aprendiendo JavaScript Moderno"
+console.log(actividad.split(" "));
+
+const hobbies = "Leer, Caminar, Escuchar musica, Escribir, Aprender a programar";
+console.log(hobbies.split(", "));
