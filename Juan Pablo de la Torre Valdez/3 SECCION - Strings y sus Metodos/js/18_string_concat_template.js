0bc9981e30d72ad1662f8cf723b2687beb3aeb3c
jreyes1990 <jarl1990mt@gmail.com>
2023-11-25 22:19:11 -0600

    18. Concatenar un String y Template Strings

---
+++ Juan Pablo de la Torre Valdez/3 SECCION - Strings y sus Metodos/js/18_string_concat_template.js
@@ -0,0 +1,6 @@
+const producto = "Monitor 20 pulgadas";
+const precio = "30 USD"
+console.log(producto+" con un precio de "+precio);
+console.log("el producto "+producto+" tiene un precio de "+precio);
+console.log("el producto ",producto," tiene un precio de ",precio);
+console.log(`el producto ${producto} tiene un precio de $ ${precio}`);
