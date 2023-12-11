8c5fa57208da602c5293ace05ce6b31a4ce8e978
jreyes1990 <jarl1990mt@gmail.com>
2023-12-10 22:07:40 -0600

    Operadores en JavaScript

---
+++ Juan Pablo de la Torre Valdez/5 SECCION - Operadores/js/31_Compara_null_y_undefined.js
@@ -0,0 +1,10 @@
+console.log('******** Undefined ********');
+let numero;
+console.log(numero);
+console.log(typeof numero);
+
+console.log('******** Null ********');
+let numero2 = null;
+console.log(numero2);
+console.log(typeof numero2);
+console.log(numero === numero2);
