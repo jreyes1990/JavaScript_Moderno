d168e306a64f2db9cf47f26963ac7f04fd268871
jreyes1990 <jarl1990mt@gmail.com>
2023-12-10 18:51:47 -0600

    25. El Objeto Math

---
+++ Juan Pablo de la Torre Valdez/4 SECCION - Numeros/js/25_Objeto_Math.js
@@ -0,0 +1,47 @@
+let resultado;
+
+// TODO: Funcion de PI
+console.log('******** Uso de Math.PI ********');
+console.log(Math.PI);
+
+// TODO: Funcion de redondear
+console.log('******** Uso de Math.round ********');
+console.log(Math.round(2.8));
+console.log(Math.round(2.2));
+console.log(Math.round(2.6));
+
+// TODO: Funcion de redondear hacia arriba
+console.log('******** Uso de Math.ceil ********');
+console.log(Math.ceil(2.1));
+
+// TODO: Funcion de redondear hacia abajo
+console.log('******** Uso de Math.floor ********');
+console.log(Math.floor(2.9));
+
+// TODO: Funcion de raiz cuadrada
+console.log('******** Uso de Math.sqrt ********');
+console.log(Math.sqrt(144));
+
+// TODO: Funcion de absoluto
+console.log('******** Uso de Math.abs ********');
+console.log(Math.abs(-500));
+
+// TODO: Funcion de Potencia
+console.log('******** Uso de Math.pow ********');
+console.log(Math.pow(2, 4));
+
+// TODO: Funcion de Minimo
+console.log('******** Uso de Math.min ********');
+console.log(Math.min(3, 5, 1, 12, -3));
+
+// TODO: Funcion de Maximo
+console.log('******** Uso de Math.max ********');
+console.log(Math.max(3, 5, 1, 12, -3));
+
+// TODO: Funcion Aleatorio
+console.log('******** Uso de Math.random ********');
+console.log(Math.random());
+
+// TODO: Funcion Aleatorio dentro de un rango
+console.log('******** Uso de Math.random dentro de un rango ********');
+console.log(Math.floor(Math.random()*30));
