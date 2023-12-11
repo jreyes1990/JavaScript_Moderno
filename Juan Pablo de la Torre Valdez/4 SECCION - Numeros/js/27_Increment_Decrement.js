ad4947d305af0279ac703580434a0778a849a95c
jreyes1990 <jarl1990mt@gmail.com>
2023-12-10 19:28:49 -0600

    27. Incrementos o Decrementos

---
+++ Juan Pablo de la Torre Valdez/4 SECCION - Numeros/js/27_Increment_Decrement.js
@@ -0,0 +1,18 @@
+let puntaje = 5;
+console.log(puntaje);
+
+console.log('******** Incremento ********');
+puntaje++;
+console.log(`puntaje++ : ${puntaje}`);
+++puntaje;
+console.log(`++puntaje : ${puntaje}`);
+puntaje += 3;
+console.log(`puntaje += : ${puntaje}`);
+
+console.log('******** Decremento ********');
+puntaje--;
+console.log(`puntaje-- : ${puntaje}`);
+--puntaje;
+console.log(`--puntaje : ${puntaje}`);
+puntaje -= 3;
+console.log(`puntaje -= : ${puntaje}`);
