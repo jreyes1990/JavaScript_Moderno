93d477908de96322bd35df25927e6fae607a38ad
jreyes1990 <jarl1990mt@gmail.com>
2023-12-10 22:50:30 -0600

    35. Crear Objetos

---
+++ Juan Pablo de la Torre Valdez/7 SECCION - Objetos/js/35_Objetos.js
@@ -0,0 +1,12 @@
+const nombre = "Monitor 20 pugadas";
+const precio = 300;
+const disponible = true;
+
+// TODO: Un objeto agrupa todo en una sola variable
+const producto = {
+  nombre: "Monitor 20 pugadas",
+  precio: 300,
+  disponible: true
+};
+
+console.log(producto);
